1dca7651c1b0f08efc1111f7d61803f93cd85974
下平裕次郎 <shimodairayuu@gmail.com>
2023-08-01 20:43:03 +0900

    middle.dartのappbarとfloatingActionButtonの追加をした

--- lib/src/screens/middle.dart
+++ lib/src/screens/middle.dart
@@ -5,6 +5,20 @@ class MiddlePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('予定一覧：中火',
+        style: TextStyle(
+          color: Colors.white,fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.black,
+     ),
+     floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        backgroundColor: Colors.redAccent,
+        child: const Icon(Icons.add_outlined),
+      ),
+    );
   }
 }
