1f16c78484844652e3562f7681bfe0142e5ae242
simanoyuki <yuukireirei5@gmail.com>
2023-08-03 22:19:29 +0900

    右上の"DEBUG"を消す

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,11 +6,11 @@ import 'package:sloth_shift/src/screens/small.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'sloth_shift',
       theme: ThemeData(
         // This is the theme of your application.
