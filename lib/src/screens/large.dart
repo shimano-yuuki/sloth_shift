746cf6a8276d658a37e16f83cbbc9b174ebaa27c
tensey3 <kikuchitensei428@gmail.com>
2023-08-03 22:37:47 +0900

    appbarと、floatignActionButtonのUIの変更

--- lib/src/screens/large.dart
+++ lib/src/screens/large.dart
@@ -6,12 +6,23 @@ class LargePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: const Text(
+          '予定一覧：強火',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.black,
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // Add your onPressed code here!
         },
-        backgroundColor: Colors.redAccent,
-        child: const Icon(Icons.add_outlined),
+        backgroundColor: Colors.black,
+        child: const Icon(
+          Icons.add_outlined,
+          color: Colors.white,
+        ),
       ),
     );
   }
