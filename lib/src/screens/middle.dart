3cb94e01a908a3796c5b343d395efb23bbbbf2d9
下平裕次郎 <shimodairayuu@gmail.com>
2023-08-28 15:34:01 +0900

    リスト表示

--- lib/src/screens/middle.dart
+++ lib/src/screens/middle.dart
@@ -6,6 +6,7 @@ class MiddlePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> TemporaryList = ['朝ごはん', '昼ごはん', '夜ご飯'];
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar(
@@ -13,13 +14,28 @@ class MiddlePage extends StatelessWidget {
         style: TextStyle(
           color: Colors.white,fontWeight: FontWeight.bold),),
         backgroundColor: Colors.black,
-     ),
-     floatingActionButton: FloatingActionButton(
+      ),
+      body: Container(
+        child: ListView.builder(
+          itemCount: TemporaryList.length,
+          itemBuilder: (BuildContext, index) {
+            return Container(
+              padding: const EdgeInsets.all(2),
+              color: Colors.black,
+              child: ListTile(
+                title: Text('${TemporaryList[index]}',
+                style: TextStyle(color: Colors.white),),
+              )
+            );
+          }
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
         onPressed: () {
-           Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => AddPage()),
-                  );
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddPage()),
+          );
         },
         backgroundColor: Colors.black,
         child: const Icon(Icons.add_outlined,color: Colors.white,),
