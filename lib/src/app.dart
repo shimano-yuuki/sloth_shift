233664ba0a71d5e8738864936d17d0a3e43b805d
tensey3 <kikuchitensei428@gmail.com>
2023-09-03 23:48:30 +0900

    ファイルにあった無駄な記述の削除

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sloth_shift/src/screens/home.dart';
-import 'package:sloth_shift/src/screens/large.dart';
-import 'package:sloth_shift/src/screens/middle.dart';
 import 'package:sloth_shift/src/screens/sign_up.dart';
-import 'package:sloth_shift/src/screens/small.dart';
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -34,76 +31,5 @@ class MyApp extends StatelessWidget {
       ),
       home: SignUpPage(),
     );
-  }
-}
-
-class SlothShift extends StatefulWidget {
-  const SlothShift({Key? key}) : super(key: key);
-
-  @override
-  State<SlothShift> createState() => _SlothShiftState();
-}
-
-class _SlothShiftState extends State<SlothShift> {
-  static const _screens = [
-    HomePage(),
-    SmallPage(),
-    MiddlePage(),
-    LargePage(),
-  ];
-
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: _screens[_selectedIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.black87,
-          currentIndex: _selectedIndex,
-          unselectedItemColor: Colors.white,
-          onTap: _onItemTapped,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: SizedBox(
-                height: 50,
-                width: 50,
-                child: Image.asset('assets/images/home.png'),
-              ),
-              label: 'ホーム',
-            ),
-            BottomNavigationBarItem(
-              icon: SizedBox(
-                height: 50,
-                width: 50,
-                child: Image.asset('assets/images/small.png'),
-              ),
-              label: '弱火',
-            ),
-            BottomNavigationBarItem(
-              icon: SizedBox(
-                height: 50,
-                width: 50,
-                child: Image.asset('assets/images/middle.png'),
-              ),
-              label: '中火',
-            ),
-            BottomNavigationBarItem(
-              icon: SizedBox(
-                height: 50,
-                width: 50,
-                child: Image.asset('assets/images/large.png'),
-              ),
-              label: '強火',
-            ),
-          ],
-          type: BottomNavigationBarType.fixed,
-        ));
   }
 }
