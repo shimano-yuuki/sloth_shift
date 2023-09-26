5eb62203ff8aa775585e1209ea890d7eba229b87
tensey3 <kikuchitensei428@gmail.com>
2023-09-26 15:34:15 +0900

    login機能

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:sloth_shift/src/app.dart';
 import 'package:sloth_shift/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-Future<void> main() async { // main 関数でも async が使えます
+Future<void> main() async {
+  // main 関数でも async が使えます
   WidgetsFlutterBinding.ensureInitialized(); // runApp 前に何かを実行したいときはこれが必要です。
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
-
